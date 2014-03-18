000b9447d273d4a57ff1f418006080bd1a93003e
raccoon <a60814billy@gmail.com>
2014-03-18 03:20:09 +0000

    [Bug fix] 1. 新增Framework.exClass     相較舊版Framework.Class 此版本繼承鏈可以到3層以上     (不過不知道有沒有Bug所以就先取另一個名字     demo 放在demo/exClass 中 2. 修正 throw 中錯字

--- core.js
+++ core.js
@@ -1,10 +1,49 @@
 // By Raccoon , undyingmoon
 var Framework = (function (Framework) {
     'use strict'
+
+    Framework.exClass = function(){
+        if(this instanceof Framework.exClass){
+            Framework.DebugInfo.Log.error('不能再Framework.exClass之前使用new關鍵字');
+            throw 'can\'t use new keyword on Framework.exClass';
+        }
+        var parent, props, childClass , i;
+        if(arguments.length === 1){
+            if(Framework.Util.isObject(arguments[0])){
+                props = arguments[0];
+                parent = Object;
+            }else{
+                Framework.DebugInfo.Log.error('Framework.exClass 參數錯誤');
+                throw 'Framework.exClass\'s argument error';
+            }
+        }else if(arguments.length === 2){
+            parent = arguments[0];
+            props = arguments[1];
+        }
+
+        childClass = function(){
+            if(Framework.Util.isUndefined(this)){
+                Framework.DebugInfo.Log.error('必須使用new關鍵字');
+                throw 'must be use new keyword';
+            }
+            if(childClass.prototype.hasOwnProperty("__construct")){
+                    childClass.prototype.__construct.apply(this, arguments);
+            }
+        }
+        childClass.prototype = new parent();
+        childClass.prototype.constructor = childClass;
+        for(i in props){
+            if(props.hasOwnProperty(i)){
+                childClass.prototype[i] = props[i];
+            }
+        }
+        return childClass;
+    };
+
     Framework.Class = function() {        
         if(this instanceof Framework.Class){
             Framework.DebugInfo.Log.error('不能在Framework.Class之前使用new關鍵字');
-            throw 'can\'t use new keywork on Framework.Class';
+            throw 'can\'t use new keyword on Framework.Class';
         }
         var parent, props , child , f , i;
         if(arguments.length === 1){
